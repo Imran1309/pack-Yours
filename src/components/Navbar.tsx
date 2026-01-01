194ec41e7c5eed9080a19be2214ed78652cfd2a5
imran.s.s <imranss1309@gmail.com>
2026-01-01 16:14:44 +0530

    Fix previous incomplete deployment

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -55,7 +55,7 @@ const Navbar = () => {
   const navItems = [
     { name: "Home", type: "hash", path: "/" },
     { name: "Packages", type: "hash", path: "#packages" },
-    { name: "States", type: "page", path: "/destination" },
+    { name: "Destination", type: "page", path: "/destination" },
     { name: "Memories", type: "page", path: "/memories" },
     { name: "Reviews", type: "hash", path: "#reviews" },
     { name: "About us", type: "hash", path: "#about" },
